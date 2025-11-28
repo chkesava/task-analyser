0025e0ea8210864588588f5058a7c266e42b3792
Kesava <chkesava54@gmail.com>
2025-11-28 19:53:41 +0530

    models: defined Task model and applied initial migration

--- tasks/models.py
+++ tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    due_date = models.DateField()
+    importance = models.IntegerField(default=5)  # 1-10
+    estimated_hours = models.FloatField(default=1.0)
+    dependencies = models.JSONField(default=list, blank=True)
+
+    def __str__(self):
+        return self.title
